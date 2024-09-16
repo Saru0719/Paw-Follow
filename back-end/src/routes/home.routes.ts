8ae70b6e7cebf785d3608d714b5033054c3f0cb4
Saru0719 <svot872@gmail.com>
2024-09-16 15:27:29 -0500

    implementación de JWT y protección de las rutas

--- back-end/src/routes/home.routes.ts
+++ back-end/src/routes/home.routes.ts
@@ -1,16 +1,23 @@
-import { Router } from 'express';
-import { Add_pets, Get_all_pets, Get_pet_byId, Update_pet, Delete_pet } from '../controllers/home';
+import { Router } from "express";
+import {
+  Add_pets,
+  Get_all_pets,
+  Get_pet_byId,
+  Update_pet,
+  Delete_pet,
+} from "../controllers/home";
+import { authRequired } from "../middleswares/validateToken";
 
 //Instanciacion de router
 const router = Router();
 
 //Rutas
-router.post('/pets', Add_pets);
+router.post("/pets", authRequired, Add_pets);
 
-router.get('/pets', Get_all_pets);
+router.get("/pets", authRequired, Get_all_pets);
 
-router.get('/pets/:id', Get_pet_byId);
+router.get("/pets/:id", authRequired, Get_pet_byId);
 
-router.put('/pets/:id', Update_pet);
+router.put("/pets/:id", authRequired, Update_pet);
 
-router.delete('/pets/:id', Delete_pet);
+router.delete("/pets/:id", authRequired, Delete_pet);
