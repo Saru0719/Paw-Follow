47f601698ee573ca92e9724b358c8e1284b0e762
Saru0719 <svot872@gmail.com>
2024-09-18 00:07:23 -0500

    Se dockerizó la aplicacion

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,9 +1,9 @@
 //Conexion a la base de datos
 import { createPool } from "mysql2/promise";
 
-export const pool = createPool ({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "paw_follow"
- });
+export const pool = createPool({
+  host: "db",
+  user: "root",
+  password: "12345",
+  database: "paw_follow",
+});
---
+++ init.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS paw_follow;
+use paw_follow;
+
+CREATE TABLE IF NOT EXISTS tbl_users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    cellphone VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS tbl_pets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type_of_pet VARCHAR(255) NOT NULL,
+    pet_name VARCHAR(255) NOT NULL,
+    date_of_birth VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    breed VARCHAR(255) NOT NULL,
+    size VARCHAR(255) NOT NULL,
+    weight INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    idOwner INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tbl_health (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pet_name VARCHAR(255),
+    date_h DATE,
+    descriptions TEXT,
+    idOwner INT NOT NULL
+);
